80897f278696b7d2440c9481e374f33a3b78fc40
Kristian <49764796+kristianka@users.noreply.github.com>
2025-07-05 00:10:06 +0300

    exercise 1.5

--- the_project/index.ts
+++ the_project/index.ts
@@ -1,7 +1,14 @@
+import mySite from "./index.html";
+
 const server = Bun.serve({
   port: Bun.env.PORT ? parseInt(Bun.env.PORT) : 3000,
-  fetch(req) {
-    return new Response("Bun!");
+  routes: {
+    "/": mySite,
+    "/hello": () => new Response("Hello, Bun!"),
+    "/json": () =>
+      new Response(JSON.stringify({ message: "Hello, JSON!" }), {
+        headers: { "Content-Type": "application/json" },
+      }),
   },
 });
 
@@ -9,3 +16,6 @@ console.log(`Server started in port ${server.port}!`);
 
 // docker build --pull -t project .
 // docker run -d -p 3000:3000 project
+
+// forwarding port in kube
+// kubectl port-forward todo-server-dep-6f574656bd-m9z28 5000:5000
